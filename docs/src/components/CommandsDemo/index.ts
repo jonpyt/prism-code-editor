931e9e3d383aed3d8a3bbaca9ee4a8f61da90b07
jonpyt <82079841+jonpyt@users.noreply.github.com>
2026-04-12 17:00:36 +0200

    Update keyboard handling documentation

--- docs/src/components/CommandsDemo/index.ts
+++ docs/src/components/CommandsDemo/index.ts
@@ -1,4 +1,4 @@
-import { getKeysFromEvent } from "prism-code-editor/commands"
+import { autoCompleteShortcutLabels, defaultKeymapLabels, formatHotkey, getKeysFromEvent, searchShortcutLabels } from "prism-code-editor/commands"
 import { editors } from "../editor/mount"
 
 const text = document.getElementById("prev-keys")!.firstChild as Text
@@ -18,3 +18,33 @@ editors[1].textarea.addEventListener(
 	},
 	true,
 )
+
+const itemTemplate = document.createElement("li")
+
+const content = document.querySelector(".sl-markdown-content")!
+
+const renderShortcuts = (title: string, list: [string, string][]) => {
+	const listEl = document.createElement("ul")
+	const titleEl = document.createElement("h4")
+
+	titleEl.textContent = title
+	listEl.className = "hotkey-list not-content"
+
+	for (const [key, description] of list) {
+		const item = itemTemplate.cloneNode(true) as HTMLLIElement
+		const [span, kbd] = item.children
+		
+		span.textContent = description
+		kbd.textContent = formatHotkey(key)
+		listEl.append(item)
+	}
+
+	content.append(titleEl, listEl)
+}
+
+itemTemplate.innerHTML = "<span></span><kbd>"
+itemTemplate.className = "hotkey-item"
+
+renderShortcuts("Code editor", defaultKeymapLabels)
+renderShortcuts("Search widget", searchShortcutLabels)
+renderShortcuts("Autocompletion", autoCompleteShortcutLabels)
