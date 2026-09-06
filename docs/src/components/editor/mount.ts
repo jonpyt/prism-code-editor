82f4b3cf021c2c418442c928f2a4a97686373a8f
Jonas Pytte <82079841+jonpyt@users.noreply.github.com>
2026-09-06 23:52:49 +0200

    Fix mounting before grammars are loaded in build

--- docs/src/components/editor/mount.ts
+++ docs/src/components/editor/mount.ts
@@ -1,8 +1,14 @@
+import type { PrismEditor } from "prism-code-editor"
 import { mountEditorsUnder } from "prism-code-editor/client"
 import { matchBrackets } from "prism-code-editor/match-brackets"
 
-const editors = mountEditorsUnder<{ _pairs?: string }>(document, options => [
-	matchBrackets(true, options._pairs),
-])
+const editors: PrismEditor<{ _pairs?: string }>[] = []
+const mount = () => {
+	editors.push(
+		...mountEditorsUnder<{ _pairs?: string }>(document, options => [
+			matchBrackets(true, options._pairs),
+		]),
+	)
+}
 
-export { editors }
+export { editors, mount }
